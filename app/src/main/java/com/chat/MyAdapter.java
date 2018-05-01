b6722dd993f77ee3b4843c121d20d09a60677f6e
Dhruv Verma <dhruv2scs@gmail.com>
2018-05-01 16:46:47 +0530

    Added image zoom animations

--- app/src/main/java/com/chat/MyAdapter.java
+++ app/src/main/java/com/chat/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.chat;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
@@ -33,14 +34,12 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         // each data item is just a string in this case
         public TextView mymessageTextView, mytimeTextView;
         public ImageView myimage;
-        public ProgressBar prog;
 
         public ViewHolder_Type1(View v) {
             super(v);
             this.mymessageTextView = (TextView) v.findViewById(R.id.mymessageTextView);
             this.mytimeTextView = (TextView) v.findViewById(R.id.mytimeTextView);
             this.myimage = (ImageView) v.findViewById(R.id.image_sent);
-            this.prog = (ProgressBar) v.findViewById(R.id.prog_rec2);
         }
     }
 
@@ -48,14 +47,12 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         // each data item is just a string in this case
         public TextView messageTextView, timeTextView;
         public ImageView recimage;
-        public ProgressBar prog1;
 
         public ViewHolder_Type2(View v) {
             super(v);
             this.messageTextView = (TextView) v.findViewById(R.id.messageTextView);
             this.timeTextView = (TextView) v.findViewById(R.id.timeTextView);
             this.recimage = (ImageView) v.findViewById(R.id.image_Rec);
-            this.prog1 = (ProgressBar) v.findViewById(R.id.prog_rec);
         }
     }
 
@@ -93,28 +90,31 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
 
         switch (getItemViewType(position)) {
 
             case VIEW_HOLDER_TYPE_1:
-                ViewHolder_Type1 viewholder1 = (ViewHolder_Type1) holder;
+                final ViewHolder_Type1 viewholder1 = (ViewHolder_Type1) holder;
                 TextView mytimeView = viewholder1.mytimeTextView;
                 mytimeView.setText(messages.get(position).getTime());
                 TextView mymsgView = viewholder1.mymessageTextView;
                 ImageView myimageView = viewholder1.myimage;
-                ProgressBar myprog = viewholder1.prog;
                 if(messages.get(position).getType().equals("image")){
-                    //myimageView.setVisibility(View.VISIBLE);
-                    myprog.setVisibility(View.VISIBLE);
-                    mymsgView.setVisibility(View.GONE);
-                    Picasso.get().load(messages.get(position).getMessage()).into(myimageView);
-                    myprog.setVisibility(View.GONE);
                     myimageView.setVisibility(View.VISIBLE);
+                    mymsgView.setVisibility(View.GONE);
+                    Picasso.get().load(messages.get(position).getMessage()).placeholder(R.drawable.progress_animation).into(myimageView);
+                    myimageView.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent in = new Intent(context, ViewImage.class);
+                            in.putExtra("img", messages.get(position).getMessage());
+                            context.startActivity(in);
+                        }
+                    });
                 }
                 else {
                     myimageView.setVisibility(View.GONE);
-                    myprog.setVisibility(View.GONE);
                     mymsgView.setVisibility(View.VISIBLE);
                     mymsgView.setClickable(true);
                     mymsgView.setMovementMethod(LinkMovementMethod.getInstance());
@@ -128,21 +128,25 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
                 break;
 
             case VIEW_HOLDER_TYPE_2:
-                ViewHolder_Type2 viewholder2 = (ViewHolder_Type2) holder;
+                final ViewHolder_Type2 viewholder2 = (ViewHolder_Type2) holder;
                 TextView timeView = viewholder2.timeTextView;
                 timeView.setText(messages.get(position).getTime());
                 TextView msgView = viewholder2.messageTextView;
                 ImageView myimageView1 = viewholder2.recimage;
-                ProgressBar prog1 = viewholder2.prog1;
                 if(messages.get(position).getType().equals("image")){
-                    prog1.setVisibility(View.VISIBLE);
-                    msgView.setVisibility(View.GONE);
-                    Picasso.get().load(messages.get(position).getMessage()).into(myimageView1);
-                    prog1.setVisibility(View.GONE);
                     myimageView1.setVisibility(View.VISIBLE);
+                    msgView.setVisibility(View.GONE);
+                    Picasso.get().load(messages.get(position).getMessage()).placeholder(R.drawable.progress_animation).into(myimageView1);
+                    myimageView1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent in = new Intent(context, ViewImage.class);
+                            in.putExtra("img", messages.get(position).getMessage());
+                            context.startActivity(in);
+                        }
+                    });
                 }
                 else {
-                    prog1.setVisibility(View.GONE);
                     myimageView1.setVisibility(View.GONE);
                     msgView.setVisibility(View.VISIBLE);
                     msgView.setClickable(true);
